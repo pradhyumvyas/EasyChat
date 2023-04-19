78ca3afe430c62c1a32b5d19a6ea5e8b9c8f1c27
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-04-20 00:50:29 +0530

    Backend completed

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+
+const server = require('http').createServer(app);
+
+const io = require('server.io')(server,{});
+
+io.on("connection",(socket)=>{
+    console.log("Socket ", socket);
+    console.log("Socket is active");
+
+    socket.on("EasyChat", (payload) =>{
+        console.log("Payload ", payload);
+        io.emit("EasyChat ", payload)
+    })
+})
+
+
+// app.listen(5000, ()=> console.log("Server is active..."));
+server.listen(5000, ()=>console.log("Server is listing at 5000..."))
