4f2a1ff643ff3609179452498c62ae593bda384f
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-04-22 02:08:02 +0530

    Addedd chat feature & added user id

--- Frontend/easychat/src/App.js
+++ Frontend/easychat/src/App.js
@@ -6,27 +6,48 @@ import {nanoid} from "nanoid";
 
 //no dotney
 const socket = io.connect("http://localhost:5000");
+const userName = nanoid(4)
 
 function App() {
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [chat, setChat] = useState([])
 
   const sendChat = (e)=>{
-    e.preventDefault()
-    socket.emit("chat", {message})
-    setMessage('');
+    e.preventDefault();
+    console.log("chat", chat);
+    socket.emit("EasyChat", {message, userName});
+    setMessage("");
   }
+
+  useEffect(()=>{
+    socket.on("EasyChat", (payload)=>{
+      console.log("helloo effect")
+      setChat([...chat, payload]);
+    });
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Easy Chat</h1>
-        <form action="" onSubmit={sendChat}>
-          <input type="text" name="chat" id="" placeholder='send text' 
+
+        {chat.map((payload, index)=>{
+          return(
+            <p key={index}>{payload.message} -- 
+            <span>id: {payload.userName}</span>
+            </p>
+          )
+        })}
+
+        <form onSubmit={sendChat}>
+          <input 
+          type="text" 
+          name="chat" 
+          placeholder='send text' 
           value={message}
           onChange={(e)=>{
-            setMessage(e.target.value)
+            setMessage(e.target.value);
           }}
           />
           <button type='submit'>Send</button>
