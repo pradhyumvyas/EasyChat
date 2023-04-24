d0b028e3cc065cbf02c0df1df9d71a4f05a2ef1b
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-04-24 23:07:55 +0530

    Local network issue fixed -- Now able to use on same network with multiple devices

--- Frontend/easychat/src/App.js
+++ Frontend/easychat/src/App.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 import {nanoid} from "nanoid";
 
 //no dotney
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("http://192.168.1.13:5000");
 const userName = nanoid(4)
 
 function App() {
@@ -29,8 +29,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Easy Chat</h1>
+        <h1 className="text-3xl font-bold py-3">Easy Chat</h1>
 
         {chat.map((payload, index)=>{
           return(
@@ -40,19 +39,20 @@ function App() {
           )
         })}
 
-        <form onSubmit={sendChat}>
-          <input 
-          type="text" 
-          name="chat" 
-          placeholder='send text' 
+        <form className='flex w-full max-w-sm items-center space-x-2' onSubmit={sendChat}>
+        <input
+          className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+          type="text"
+          placeholder="Write a message" 
           value={message}
           onChange={(e)=>{
-            setMessage(e.target.value);
-          }}
-          />
-          <button type='submit'>Send</button>
+          setMessage(e.target.value);
+          }}/>
+        <button type='submit'
+            className="active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-indigo-600 focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-indigo-700 dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-indigo-600 text-white hover:bg-indigo-700 h-10 py-2 px-4">
+            Send
+        </button>
         </form>
-      </header>
     </div>
   );
 }
---
+++ Frontend/easychat/src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div class="flex w-full max-w-sm items-center space-x-2">
+            <input
+                class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                type="email"
+                placeholder="Email" />
+            <button
+                class="active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-indigo-600 focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-indigo-700 dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-indigo-600 text-white hover:bg-indigo-700 h-10 py-2 px-4">
+                Subscribe
+            </button>
+        </div>
+    )
+}
+
+export default Home
